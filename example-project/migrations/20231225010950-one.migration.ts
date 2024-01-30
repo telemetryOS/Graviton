a65bf6147935d08bf82a9efab637ed83b66da4e5
Robert Hurst <robertwhurst@gmail.com>
2024-01-29 18:22:09 -0800

    expand first migration example

--- example-project/migrations/20231225010950-one.migration.ts
+++ example-project/migrations/20231225010950-one.migration.ts
@@ -1,7 +1,17 @@
 export function up(db: Handle) {
-  db.collection('test').insertOne({ _id: new ObjectId('65b8077faddfba1bb64fa9fe'), name: 'one' })
+  db.collection('test').insertOne({
+    _id: new ObjectId('65b8077faddfba1bb64fa9fe'),
+    name: 'one',
+    metaInfo: {
+      size: 42,
+      tags: ["foo", "bar"],
+    },
+    addedAt: new Date()
+  })
 }
 
 export function down(db: Handle) {
-  db.collection('test').deleteOne({ _id: new ObjectId('65b8077faddfba1bb64fa9fe') })
+  db.collection('test').deleteOne({
+    _id: new ObjectId('65b8077faddfba1bb64fa9fe')
+  })
 }
